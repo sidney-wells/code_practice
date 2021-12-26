00e692649736d99790247f016125a192638ecc9a
Sidney Wells <sidney@Sidneys-MacBook-Air.local>
2021-12-26 14:25:21 -0600

    flatten.js practice

--- recursion/flatten.js
+++ recursion/flatten.js
@@ -1,7 +1,11 @@
-function flatten(arrOfArrays){
+function flatten(arrOfArrays) {
     // add whatever parameters you deem necessary - good luck!
+
+    if (arrOfArrays.length === 0) return arrOfArrays;
     let newArray = [];
     for (let i = 0; i < arrOfArrays.length; i++) {
+        // if the element is a nested array,
+        // then we need to search through the array
         if (Array.isArray(arrOfArrays[i])) {
             newArray = newArray.concat(flatten(arrOfArrays[i]))
         } else {
@@ -9,9 +13,9 @@ function flatten(arrOfArrays){
         }
     }
     return newArray;
-  }
-  
-  console.log(flatten([1, 2, 3, [4, 5] ])) // [1, 2, 3, 4, 5]
-  // console.log(flatten([1, [2, [3, 4], [[5]]]])) // [1, 2, 3, 4, 5]
-  // console.log(flatten([[1],[2],[3]])) // [1,2,3]
-  // console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])) // [1,2,3
+}
+
+console.log(flatten([1, 2, 3, [4, 5]])) // [1, 2, 3, 4, 5]
+console.log(flatten([1, [2, [3, 4], [[5]]]])) // [1, 2, 3, 4, 5]
+console.log(flatten([[1], [2], [3]])) // [1,2,3]
+console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])) // [1,2,3
