fc568012239caccec17608f5b7e5cc2fc56155e4
Sidney Wells <sidney@Sidneys-MacBook-Air.local>
2021-12-28 09:26:08 -0600

    naive string search practice

--- searching/naive_string_search/naiveString.js
+++ searching/naive_string_search/naiveString.js
@@ -1,16 +1,37 @@
 function naiveStringSearch(longStr, pattern) {
   // count increment
-  let count = 0;
+
   // loop over long string
-  for (let i = 0; i < longStr.length; i++) {
-    // loop over short string
-    for (let j = 0; j < pattern.length; j++) {
-      // if long string char equals short string char, yay
-      if (pattern[j] !== longStr[i + j]) break;
-      if (j === pattern.length - 1) count++;
-    }
-  }
-  return count;
+
+  // loop over short string
+
+  // if longString not equal to pattern, break
+
+  // if long string char equals short string char, yay, count plus 1
+
+  // return count
+
+
+
+
+
+
+
+
+
+
+  // // count increment
+  // let count = 0;
+  // // loop over long string
+  // for (let i = 0; i < longStr.length; i++) {
+  //   // loop over short string
+  //   for (let j = 0; j < pattern.length; j++) {
+  //     // if long string char equals short string char, yay
+  //     if (pattern[j] !== longStr[i + j]) break;
+  //     if (j === pattern.length - 1) count++;
+  //   }
+  // }
+  // return count;
 }
 
 console.log(naiveStringSearch("helloshortshortlad", "short"))
