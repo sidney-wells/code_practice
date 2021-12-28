927b542f36c0fc91c39f2f5d16b9525e07ce3f73
Sidney Wells <sidneywells101@gmail.com>
2021-12-28 09:31:55 -0600

    naive searching practice

--- searching/naive_string_search/naiveString.js
+++ searching/naive_string_search/naiveString.js
@@ -1,24 +1,21 @@
 function naiveStringSearch(longStr, pattern) {
   // count increment
-
+  let count = 0;
   // loop over long string
-
-  // loop over short string
-
-  // if longString not equal to pattern, break
-
-  // if long string char equals short string char, yay, count plus 1
-
-  // return count
-
-
-
-
-
-
-
-
-
+  for (let i = 0; i < longStr.length; i++) {
+    // loop over short string
+    for (let j = 0; j < pattern.length; j++) {
+      // if longString not equal to pattern, break
+      if (pattern[j] !== longStr[i + j]) {
+        break;
+      }
+      // if long string char equals short string char, yay, count plus 1
+      if (j === pattern.length - 1) {
+        count++;
+      }
+    }
+  }
+  return count;
 
   // // count increment
   // let count = 0;
@@ -34,5 +31,6 @@ function naiveStringSearch(longStr, pattern) {
   // return count;
 }
 
-console.log(naiveStringSearch("helloshortshortlad", "short"))
+console.log(naiveStringSearch("helloshortshortlad", "short")); // 2
+console.log(naiveStringSearch("lololololol", "lol")); // 5
 
