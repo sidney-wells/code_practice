465fed61a98ffeb0145c6f6625d1913c22417fdf
Sidney Wells <sidneywells101@gmail.com>
2021-12-29 09:44:58 -0600

    starting sorting

--- searching/naive_string_search/naiveString.js
+++ searching/naive_string_search/naiveString.js
@@ -16,19 +16,6 @@ function naiveStringSearch(longStr, pattern) {
     }
   }
   return count;
-
-  // // count increment
-  // let count = 0;
-  // // loop over long string
-  // for (let i = 0; i < longStr.length; i++) {
-  //   // loop over short string
-  //   for (let j = 0; j < pattern.length; j++) {
-  //     // if long string char equals short string char, yay
-  //     if (pattern[j] !== longStr[i + j]) break;
-  //     if (j === pattern.length - 1) count++;
-  //   }
-  // }
-  // return count;
 }
 
 console.log(naiveStringSearch("helloshortshortlad", "short")); // 2
